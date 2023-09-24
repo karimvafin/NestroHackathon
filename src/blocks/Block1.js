0f663103e96c2b065cf686a8c3c133b75c7c9c87
boketto0 <bekmina.julia@gmail.com>
2023-09-24 14:36:08 +0300

    changes

--- src/blocks/Block1.js
+++ src/blocks/Block1.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import React, { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 
-export const Block1 = ({ points, onMarkerClick }) => {
+export const Block1 = ({ points }) => {
+  const apiKey = 'AIzaSyAYMBaXIt_-PtQ2W2_oRMBDde022yKLNcA';
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    const map = new window.google.maps.Map(mapRef.current, {
+      center: { lat: 50.906, lng: 34.793991999999996 },
+      zoom: 10,
+      mapTypeControl: false, 
+    });
+
+    points.forEach((point) => {
+      new window.google.maps.Marker({
+        position: { lat: point.lat, lng: point.lng },
+        map: map,
+        title: point.label,
+      });
+    });
+  }, [points]);
+
   return (
     <div className="block1">
-      <MapContainer center={[50.906, 34.793991999999996]} zoom={13} style={{ width: '800px', height: '600px' }} preferCanvas={true}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
+      <div
+        ref={mapRef}
+        style={{ width: '800px', height: '600px', border: '0' }}
+      ></div>
+      <div>
         {points.map((point) => (
-          <Marker
-            key={point.id}
-            position={[point.lat, point.lng]}
-            onClick={() => onMarkerClick(point.id)}
-          >
-            <Popup>{point.label}</Popup>
-          </Marker>
+          <div key={point.id}>
+            {/* <Link to={`/results/${point.id}`}>{point.label}</Link> */}
+            <Link to={`/results`}>{point.label}</Link>
+          </div>
         ))}
-      </MapContainer>
+      </div>
     </div>
   );
 };
--- src/blocks/MapContainer.js
+++ src/blocks/MapContainer.js
@@ -1,23 +1,7 @@
 import React from 'react';
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
-import { TileLayer, Popup } from 'react-leaflet';
 
-export const MapContainer =({ center, zoom, points, onMarkerClick }) => {
-    return (
-      <Map center={center} zoom={zoom} style={{ height: '600px', width: '800px' }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        {points.map((point) => (
-          <Marker
-            key={point.id}
-            position={[point.lat, point.lng]}
-            onClick={() => onMarkerClick(point.id)}
-          >
-            <Popup>{point.label}</Popup>
-          </Marker>
-        ))}
-      </Map>
-    );
-  };
+export const MapContainer = ({ center, zoom, points, onMarkerClick }) => {
+  return (
+    <div id="map" style={{ height: '600px', width: '800px' }}></div>
+  );
+};
